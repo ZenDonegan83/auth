2c7c91738c903a03187faab262e128d64f01ceb3
ZenDonegan83 <zendonegan@gmail.com>
2022-11-02 20:24:56 +0000

    added event resource

--- src/main/java/com/inkd/auth/entity/Event.java
+++ src/main/java/com/inkd/auth/entity/Event.java
@@ -6,11 +6,72 @@ import javax.persistence.*;
 @Table(name="event")
 public class Event {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="cost")
     private float cost;
+
+    @Column(name="tattoo_location", nullable = false, length = 50)
     private String tattooLocation;
+
+    @Column(name="comments", length = 1200)
     private String comments;
+
+    @Column(name="cancelled")
     private boolean cancelled;
+
+    @Column(name="no_show")
     private boolean noShow;
 
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public String getTattooLocation() {
+        return tattooLocation;
+    }
+
+    public void setTattooLocation(String tattooLocation) {
+        this.tattooLocation = tattooLocation;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    public boolean isNoShow() {
+        return noShow;
+    }
+
+    public void setNoShow(boolean noShow) {
+        this.noShow = noShow;
+    }
 }
--- src/main/java/com/inkd/auth/entity/User.java
+++ src/main/java/com/inkd/auth/entity/User.java
@@ -1,12 +1,9 @@
 package com.inkd.auth.entity;
 
-
-import lombok.Data;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+
 @Entity
 @Table(name="users")
 public class User {
@@ -40,7 +37,90 @@ public class User {
     @Transient
     private String accessToken;
 
+
+
+
+
     @Transient
     private String refreshToken;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 }
